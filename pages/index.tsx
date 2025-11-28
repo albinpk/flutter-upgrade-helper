3542da612d952ec812347af43f9bbd2def627690
Albin <56157868+albinpk@users.noreply.github.com>
2025-11-29 01:12:27 +0530

    feat: add flutter `3.13.9`, `3.10.6` and `3.7.12`

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,11 +17,34 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
+const compareSemanticVersions = (a: string, b: string) => {
+  // 1. Split the strings into their parts.
+  const a1 = a.split(".");
+  const b1 = b.split(".");
+  // 2. Contingency in case there's a 4th or 5th version
+  const len = Math.min(a1.length, b1.length);
+  // 3. Look through each version number and compare.
+  for (let i = 0; i < len; i++) {
+    const a2 = +a1[i] || 0;
+    const b2 = +b1[i] || 0;
+
+    if (a2 !== b2) {
+      return a2 > b2 ? 1 : -1;
+    }
+  }
+
+  // 4. We hit this if the all checked versions so far are equal
+  //
+  return b1.length - a1.length;
+};
+
 export async function getStaticProps() {
   const jsonPath = path.join(process.cwd(), "public", "data", "versions.json");
   const content = fs.readFileSync(jsonPath, "utf8");
   const versions = JSON.parse(content);
-  return { props: { versions: versions.reverse() } };
+  return {
+    props: { versions: versions.sort(compareSemanticVersions).reverse() },
+  };
 }
 
 export default function Home({ versions }: { versions: string[] }) {

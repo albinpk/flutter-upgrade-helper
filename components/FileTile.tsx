a56b8c9aac1596ceeb26071cd1e3dd77243ce367
Albin <56157868+albinpk@users.noreply.github.com>
2025-12-01 11:38:39 +0530

    feat: sticky file header

--- components/FileTile.tsx
+++ components/FileTile.tsx
@@ -18,7 +18,7 @@ export default function FileTile({
   const i = "text-black/50 dark:text-white/50";
   return (
     <div className="mb-5 rounded-md border border-solid border-black/20 dark:border-white/20">
-      <div className="flex flex-row items-center gap-2 rounded-md bg-black/10 dark:bg-white/10">
+      <div className="sticky top-0 flex flex-row items-center gap-2 rounded-md bg-gray-100 dark:bg-gray-900">
         <IconButton
           icon={
             visible ? (
